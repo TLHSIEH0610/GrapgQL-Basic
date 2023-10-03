32ef7cc1f9f38b801895f54abea05393e3b92cf9
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-10-03 16:37:07 +1100

    add typeDefs and resolvers

---
+++ dist/schema.js
@@ -0,0 +1,29 @@
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+// ex: The "Game" type defines the queryable fields for every game in data source.
+//  The "Query" type is special: it lists all of the available queries that
+//  clients can execute, along with the return type for each.
+//  Ex: The "games" query returns an array of zero or more games.
+export const typeDefs = `#graphql
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+  type Query {
+    games: [Game]
+    reviews: [Review]
+    authors: [Author]
+  }
+`;
---
+++ src/resolver.ts
@@ -0,0 +1,18 @@
+import db from "./_db.js";
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves games from the "games" array.
+
+export const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
